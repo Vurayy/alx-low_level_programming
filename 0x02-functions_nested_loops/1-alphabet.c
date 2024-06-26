3f5f1d5e998edf154f10b0e1c08665c35e344a74
vurayy <vurebari@outlook.com>
2024-06-25 21:42:13 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - funtion prints the alphabet in lower case
+ * Returm: return 0 (success)
+ */
+
+void print_alphabet(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+
+	}
+	_putchar('\n');
+}
