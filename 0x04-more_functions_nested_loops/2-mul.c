847d887dc0b0429a252f0e57672fc5c83da5f8a2
vurayy <vurebari@outlook.com>
2024-06-28 22:39:35 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/***
+ * mut - checks if the function multiplies two integers
+ * @a: The first number to be multiplied
+ * @b: The second number to be multiplied
+ *
+ * Return: The result of the multiplication
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
