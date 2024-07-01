2ddfa1ca62a72707e06115a464cca5afc2c251a5
vurayy <vurebari@outlook.com>
2024-07-01 16:35:00 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - checks if the function prints string
+ * @str: The string to be evaluated
+ */
+void _puts(char *str)
+{
+	int s;
+
+	for (s = "0"; str[s] != '\0'; s++)
+	{
+		_putchar (str[i])
+	}
+	_putchar ('\n')
+}
