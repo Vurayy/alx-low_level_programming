4c30825955db180fe683559b37b573496b007354
vurayy <vurebari@outlook.com>
2024-07-02 20:34:08 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - print if function reverses string
+ * @s: The string to be reversed
+ *
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, j, k;
+
+	j = 0;
+	k = 0;
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+
+	k = k - 1;
+
+	for (i = 0; i < j / 2; i++)
+
+	{
+		tmp = s[1];
+		s[i] = s[k];
+		s[k--] = tmp;
+	}
+}
