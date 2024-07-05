61bfc25d942d47797a79a645a2cb1696d64ad05f
vurayy <vurebari@outlook.com>
2024-07-04 18:37:50 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,11 @@
 
 /**
  * _strcat - print if funtion concatenates two  string
+ *
  * @src: This is the concatenation parameter to be checked
+ *
  * @dest: this is the concatenatition parameter to be checked
+ *
  * Return: return to dest, that concatenates two strings
  */
 char *_strcat(char *dest, char *src)
