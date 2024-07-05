a1042b548b7f6752d355fa813e6c5e34e809a3cc
vurayy <vurebari@outlook.com>
2024-07-04 18:27:29 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - print if funtion concatenates two  string
+ * @src: This is the concatenation parameter to be checked
+ * @dest: this is the concatenatition parameter to be checked
+ * Return: return to dest, that concatenates two strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\n')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+strcat, strncat, strcmp, strcpy, always loool up, expect the best
