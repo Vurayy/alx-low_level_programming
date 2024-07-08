96f66b1f3b41a1003f79736175e32fe8933f6001
vurayy <vurebari@outlook.com>
2024-07-08 16:09:28 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "unistd.h"
+/**
+ * __putchar - writes the charecter to stdout
+ * @c: The charac ter to print 
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1,&c, 1));
+}
