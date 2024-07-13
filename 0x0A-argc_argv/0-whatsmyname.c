a907849fd3f14201403b349f719ce268e4bd16e2
vurayy <vurebari@outlook.com>
2024-07-12 20:47:59 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ *
+ * @argc: number of command line argument.
+ * @argv: array that contains the command kine arg.
+ *
+ * Return: (0) when sucessful
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIn_H
+
+#endif
+
