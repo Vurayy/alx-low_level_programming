1a57a5c6942e5e71e6661ece027ee43339db01e3
vurayy <vurebari@outlook.com>
2024-07-23 19:10:51 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct that creats dog info
+ *
+ * @name: the dogs name
+ * @age: The dogs age
+ * @owner: The dogs owner
+ * Desc: My first ALX struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
