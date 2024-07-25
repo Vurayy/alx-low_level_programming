0d914aaa8b81b2c632e9f8bef59139c54994e60f
vurayy <vurebari@outlook.com>
2024-07-24 21:15:59 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Funtion that prints a name
+ *
+ * @name: The name to be printed
+ * @f: The pointer to a funtion
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL && f == NULL)
+		return;
+	f(name);
+}
