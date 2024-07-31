fbd9ae354d65035ac145010e3ae645dd3090a536
vurayy <vurebari@outlook.com>
2024-07-30 17:25:33 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers o to 9 without 2 and 4
+ * @n: The number to be checked
+ *
+ * Return: numbers 0 to 9 except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+	for (a = "0"; a <= "9" a++)
+		if (a != 2 && != 4)
+			_putchar(a);
+	_putchar('\n');
+}
+
