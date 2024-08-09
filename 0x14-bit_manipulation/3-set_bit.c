73abb992451b9b4983e73f0e65df03c734022b15
vurayy <vurebari@outlook.com>
2024-08-08 21:22:28 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@
  * set_bit - function that sets the value of a bit to 1 at a given index
  * @n: pointer to an integer
  * @index: index to set to 1
- * Returns: 1 on success otherwise -1
+ * Return: 1 on success and -1 on failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
